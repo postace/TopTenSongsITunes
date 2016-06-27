e8bb0611c461deb72bf0032a4fe5b98a3ad11cf4
Hai <thanhhai.fu@gmail.com>
2016-06-27 15:37:57 +0700

    Reference layout and java variables

--- app/src/main/java/com/postace/top10songsitunes/MainActivity.java
+++ app/src/main/java/com/postace/top10songsitunes/MainActivity.java
@@ -2,12 +2,23 @@ package com.postace.top10songsitunes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String URL_APPLE_RSS_FEEDS = "http://ax.itunes.apple.com/" +
+            "WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=10/xml";
+    private Button btnParse;
+    private ListView xmlListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnParse = (Button) findViewById(R.id.btnParse);
+        xmlListView = (ListView) findViewById(R.id.xmlListView);
     }
 }
