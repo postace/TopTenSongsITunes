d98264811164710785743816622bb25b33490609
Hai <thanhhai.fu@gmail.com>
2016-06-27 15:43:55 +0700

    Adding private class DownloadData and Override doInBackground() method.

--- app/src/main/java/com/postace/top10songsitunes/MainActivity.java
+++ app/src/main/java/com/postace/top10songsitunes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.postace.top10songsitunes;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
 
         btnParse = (Button) findViewById(R.id.btnParse);
         xmlListView = (ListView) findViewById(R.id.xmlListView);
+    }
+
+    /**
+     * Class DownloadData
+     * @return: XML string represent list 10 songs
+     */
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        private String mFileContents;
+
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXMLFile(params[0]);
+            if (mFileContents == null) {
+                Log.d("DownloadData","An error occur");
+            }
+
+            return mFileContents;
+        }
+
     }
 }
